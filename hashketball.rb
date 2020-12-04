cd1dec72888970e67810a480e9c6a85b1d4347fd
Melissa F <melissa.faulner@gmail.com>
2020-12-04 18:41:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -137,7 +137,7 @@ def num_points_scored(player_name)
   # all_players_helper[:player][:score]
   game_hash.each do |home_away, keys_list|
     keys_list[:players].each do |player|
-      return player[:points] player
+      return player[:points] player[:player_name] ==
 end
 
 def shoe_size(player_name)
