dc504d9abd33a980365ff56cc9bc1a108d01add7
Melissa F <melissa.faulner@gmail.com>
2020-12-04 18:42:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -139,7 +139,7 @@ def num_points_scored(player_name)
     keys_list[:players].each do |player|
       return player[:points] player[:player_name] == player_name
     end
-  end
+  
 end
 
 def shoe_size(player_name)
@@ -186,4 +186,4 @@ end
 def long_name_steals_a_ton
   #returns True if the player w/ longest name has most steals
 end
-end
+
