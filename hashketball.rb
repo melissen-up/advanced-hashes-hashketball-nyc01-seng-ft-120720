4ebb5b977686526be693233902162bbc955d7607
Melissa F <melissa.faulner@gmail.com>
2020-12-04 18:47:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -153,6 +153,8 @@ end
 
 def team_colors(team_name)
   #takes in a team name and returns an Array of team's colors
+  game_hash.each do |home_away, keys_list|
+    if keys_list
 end
 
 def team_names
