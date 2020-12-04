56ec209ecef488778a2980ffffab2ce82595385e
Melissa F <melissa.faulner@gmail.com>
2020-12-04 18:43:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -139,7 +139,6 @@ def num_points_scored(player_name)
       return player[:points] player[:player_name] == player_name
     end
   end
-end
 
 def shoe_size(player_name)
   #takes in a player's name and returns shoe size
